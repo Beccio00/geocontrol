96af5fd9eca5e8ac101abadcb70b3aa3d2438f7f
Beccio00 <riccardo.becciolini00@gmail.com>
2025-05-07 21:46:27 +0200

    feat(netdao): init the implementation of network dao

--- src/models/dao/NetworkDAO.ts
+++ src/models/dao/NetworkDAO.ts
@@ -1,3 +1,15 @@
+import { Entity, PrimaryColumn, Column } from "typeorm";
+
+@Entity("networks")
 export class NetworkDAO {
-    
+    @PrimaryColumn({ nullable: false })
+    code: string;
+
+    @Column({ nullable: false })
+    name: string;
+
+    @Column({ nullable: false })
+    description: string;
+
+    //TODO: Add relation to GatewayDAO
 }
